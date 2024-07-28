1a49e8cde687e30bde24d2fa27d79dbe565e7eea
Jeph Mari Daligdig <daligdigjeph09@gmail.com>
2024-07-28 13:39:37 +0800

    add startGame method

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,50 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+class Game {
+    constructor() {
+        this.missed = 0;
+        this.phrases = [
+            new Phrase('Life is like a box of chocolates'),
+            new Phrase('There is no place like home'),
+            new Phrase('The early bird catches the worm'),
+            new Phrase('To infinity and beyond'),
+            new Phrase('May the force be with you')
+        ];
+        this.activePhrase = null;
+    }
+
+    /**
+    * Selects random phrase from phrases property
+    * @return {Object} Phrase object chosen to be used
+    */
+    getRandomPhrase() {
+        let randomIndex = Math.floor(Math.random() * this.phrases.length);
+        return this.phrases[randomIndex];
+    };
+
+    /**
+    * Begins game by selecting a random phrase and displaying it to user
+    */
+    startGame() {
+        const overlay = document.getElementById('overlay');
+        this.activePhrase = this.getRandomPhrase();
+        this.activePhrase.addPhraseToDisplay();
+    }
+
+    handleInteraction() {
+
+    }
+
+    removeLife() {
+
+    }
+
+    checkForWin() {
+
+    }
+
+    gameOver() {
+
+    }
+}
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,39 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+class Phrase {
+    constructor(phrase) {
+        this.phrase = phrase.toLowerCase();
+    }
+
+    /**
+    * Display phrase on game board
+    */
+    addPhraseToDisplay() {
+        const phraseDiv = document.getElementById('phrase');
+        const letters = this.phrase.split('');
+        const ul = phraseDiv.querySelector('ul');
+
+        ul.innerHTML = '';
+
+        letters.forEach(letter => {
+            const li = document.createElement('li');
+            if (letter !== ' ') {
+                li.className = `hide letter ${letter}`;
+                li.textContent = letter;
+            } else {
+                li.className = 'space';
+                li.textContent = ' ';
+            }
+            ul.appendChild(li);
+        });
+    }
+
+    checkLetter() {
+
+    }
+
+    showMatchedLetter() {
+
+    }
+}
